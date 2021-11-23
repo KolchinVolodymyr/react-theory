39d91ea9a0607da8acd69e29a60b964200b65fdb
Volodymyr Kolchin <vkolchin@codifi.me>
2021-11-23 15:08:28 +0200

    Creating a simple component

---
+++ src/Car/Car.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//function Car() {
+//    return (
+//        <h2>This is car component</h2>
+//    )
+//}
+const car = () => {
+    return (
+        <h2>This is car component</h2>
+    )
+}
+
+export default car
