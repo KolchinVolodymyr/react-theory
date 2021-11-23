74a2ed2755b8dee9da1da2730d8f9e986c0da84b
Volodymyr Kolchin <vkolchin@codifi.me>
2021-11-23 18:23:26 +0200

    added inline styles

--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
 export default props =>  (
-    <div>
+    <div style={{
+             border: '1px solid #ccc',
+             marginBottom: '10px',
+             display: 'block',
+             padding: '10px',
+             boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
+             borderRadius: '5px',
+             width: 400,
+             margin: '20px auto',
+             paddingTop: '20px'
+           }}>
         <h2>Car name: {props.name}</h2>
         <p>Year: <strong>{props.year}</strong></p>
         <input type="text" onChange={props.onChangeName} value={props.name}/>
